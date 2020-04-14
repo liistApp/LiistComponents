d7a35f0f1e4ae47fbdf40552ad3657da80483eb2
Thomas Starzynski <thomas.starzynski@unibas.ch>
2020-04-14 22:37:50 +0200

    save docs

---
+++ src/LiistColors.js
@@ -0,0 +1,14 @@
+// How to import variable
+// import { listColors } from '../LiistColors.js';
+
+export const liistColors = {
+  sunii: "#F2D70B",
+  skii: "##337EF8",
+  viiolet: "#4F51C2",
+  grasii: "#18943F",
+  hotii: "#18943F",
+  piink: "#FAC8DA",
+  creamii: "#F9DDC5",
+  smokii: "#1A1A1A"
+};
+
