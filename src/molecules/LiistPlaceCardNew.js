e92832795a097566787442e549c849c378d411dd
David Friedl <david.friedl@whu.edu>
2020-07-02 11:41:50 +0200

    test place card new

---
+++ src/molecules/LiistPlaceCardNew.js
@@ -0,0 +1,26 @@
+import { html, css, LitElement } from 'lit-element';
+
+export class LiistAuthor extends LitElement {
+  static get styles() {
+    return css`
+
+    `;
+  }
+
+  static get properties() {
+    return {
+    };
+  }
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return html`
+
+    `;
+  }
+}
+
+window.customElements.define('liist-place-card-new', LiistPlaceCardNew);
