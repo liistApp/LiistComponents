738f8328321b2225dea2719be3802b7a5980051b
Thomas Starzynski <thomas.starzynski@unibas.ch>
2020-07-14 10:37:43 +0200

    export all components

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+export { LiistColors } from './src/LiistColors.js';
 /*
  * ATOMS
  */
 export { LiistBttn } from './src/atoms/LiistBttn.js';
+export { LiistBttnV2 } from './src/atoms/LiistBttnV2.js';
 export { LiistDaadBttn } from './src/atoms/LiistDaadBttn.js';
+export { LiistSection } from './src/atoms/LiistSection.js';
 /*
  * MOLECULES
  */
 export { LiistAuthor } from './src/molecules/LiistAuthor.js';
 export { LiistCheckItem } from './src/molecules/LiistCheckItem.js';
 export { LiistPlaceCard } from './src/molecules/LiistPlaceCard.js';
+export { LiistPlaceCardNew } from './src/molecules/LiistPlaceCardNew.js';
 export { LiistToggleBttn } from './src/molecules/LiistToggleBttn.js';
 /*
  * ORGAMISMS
