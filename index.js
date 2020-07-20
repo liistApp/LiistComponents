095fcb9419775f1d73a3535441ad4885810dba66
dcts <thomas@liist.com>
2020-07-20 02:42:41 +0200

    remove unneeded files for release V1.0.0

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 // export { LiistBttnOld } from './src/atoms/LiistBttn.js';
 export { LiistColors } from './src/atoms/LiistColors.js';
 export { LiistBttn } from './src/atoms/LiistBttn.js';
-export { LiistBttnOld } from './src/atoms/LiistBttnOld.js';
 export { LiistDaadBttn } from './src/atoms/LiistDaadBttn.js';
 export { LiistIcon } from './src/atoms/LiistIcon.js';
 /*
@@ -13,9 +12,9 @@ export { LiistIcon } from './src/atoms/LiistIcon.js';
 export { LiistAuthor } from './src/molecules/LiistAuthor.js';
 export { LiistCheckItem } from './src/molecules/LiistCheckItem.js';
 export { LiistPlaceCard } from './src/molecules/LiistPlaceCard.js';
-export { LiistPlaceCardOld } from './src/molecules/LiistPlaceCardOld.js';
 export { LiistSection } from './src/molecules/LiistSection.js';
 export { LiistToggleBttn } from './src/molecules/LiistToggleBttn.js';
+export { LiistTag } from './src/molecules/LiistTag.js';
 /*
  * ORGAMISMS
  */
